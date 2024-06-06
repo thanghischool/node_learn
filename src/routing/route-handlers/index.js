f3895f262dd999b570f2f901ebc1c85742a2e80b
thanghischool <thangxpro999@gmail.com>
2024-06-06 18:21:42 +0700

    Lab 4

---
+++ src/routing/route-handlers/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get(
+  '/example/b',
+  (req, res, next) => {
+    console.log('the response will be sent by the next function ...');
+    next();
+  },
+  (req, res) => {
+    res.send('Hello from B!');
+  }
+);
+
+const cb0 = function (req, res, next) {
+  console.log('CB0');
+  next();
+};
+
+const cb1 = function (req, res, next) {
+  console.log('CB1');
+  next();
+};
+
+app.get(
+  '/example/d',
+  [cb0, cb1],
+  (req, res, next) => {
+    console.log('the response will be sent by the next function ...');
+    next();
+  },
+  (req, res) => {
+    res.send('Hello from D!');
+  }
+);
+
+app.listen(port, () => {
+  console.log(`Example app listening http://localhost:${port}`);
+});
+
+// npm run dev
---
+++ src/routing/route-params/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/users/:userId/books/:bookId', (req, res) => {
+  res.send(req.params);
+});
+
+app.get('/plantae/:genus.:species', (req, res) => {
+  res.send(req.params);
+});
+
+app.get('/user/:userId(\d+)', (req, res) => {
+  res.send(req.params);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening http://localhost:${port}`);
+});
+
+// npm run dev
